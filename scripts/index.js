df675d24d7e86e2ae373ece9e2c9151e1e46346d
Ale Tsayder <goldrigma@gmail.com>
2022-09-24 15:32:36 -0300

    fix: убрать display из popup

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,13 @@ const popupOpenButton = document.querySelector('.profile__open-popup');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 const popupSaveButton = popup.querySelector('.popup__button')
-const popupToggle = function() {
-    popup.classList.toggle('popup_opened');   
+const popupToggle = function () {
+  popup.classList.toggle('popup_opened');
 };
+
+function popupOpen() {
+  person.classList.remove('popup_opened');
+}
 
 let formElement = document.querySelector('.popup__content');
 let nameInput = popup.querySelector('.popup__form_name');
@@ -13,13 +17,13 @@ let jobInput = popup.querySelector('.popup__form_profession');
 let person = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-      person.textContent = nameInput.value;
-      about.textContent = jobInput.value;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  person.textContent = nameInput.value;
+  about.textContent = jobInput.value;
 }
 
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 popupSaveButton.addEventListener('click', popupToggle);
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
