142c428d42a2b4b233bd64d601c3c92079cf5ac8
Ale Tsayder <goldrigma@gmail.com>
2022-09-28 15:08:12 -0300

    fix: осуществить правки

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popupOpenButton = document.querySelector('.profile__open-popup');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__content');
-let nameInput = popup.querySelector('.popup__content__popup__form-text_name_input');
-let jobInput = popup.querySelector('.popup__content__popup__form-text_about_input');
+let nameInput = popup.querySelector('.popup__popup__form-text_name_input');
+let jobInput = popup.querySelector('.popup__popup__form-text_about_input');
 let person = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
 
