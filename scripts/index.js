09ed7e1c8c872401d21145cdd2db94bcdfe1aaee
Ale Tsayder <goldrigma@gmail.com>
2022-09-28 14:56:55 -0300

    fix: осуществить правки везде

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
-// получить элементы
 const popupOpenButton = document.querySelector('.profile__open-popup');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
-const popupSaveButton = popup.querySelector('.popup__button')
-const popupToggle = function () {
-  popup.classList.toggle('popup_opened');
-};
-
-function popupOpen() {
-  person.classList.remove('popup_opened');
-}
-
 let formElement = document.querySelector('.popup__content');
-let nameInput = popup.querySelector('.popup__form-text_name');
-let jobInput = popup.querySelector('.popup__form-text_profession');
+let nameInput = popup.querySelector('.popup__content__popup__form-text_name_input');
+let jobInput = popup.querySelector('.popup__content__popup__form-text_about_input');
 let person = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
+
+const popupToggle = function () {
+  popup.classList.toggle('popup_opened');
+}
+
+const closePopup = function () {
+  popup.classList.toggle('popup_opened');
+}
+
+function formFields() {
+  nameInput.value = person.textContent;
+  jobInput.value = about.textContent;
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   person.textContent = nameInput.value;
   about.textContent = jobInput.value;
+  closePopup();
 }
-
 popupOpenButton.addEventListener('click', popupToggle);
+popupOpenButton.addEventListener('click', formFields);
 popupCloseButton.addEventListener('click', popupToggle);
-popupSaveButton.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', formSubmitHandler);
+
